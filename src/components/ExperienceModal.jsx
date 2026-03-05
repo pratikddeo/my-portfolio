a2da4aead4902f2ac503ca1010430fcef72436de
Pratik Deo <pratik1307xy@gmail.com>
2026-03-05 21:48:39 +0100

    Fix asset paths with BASE_URL

--- src/components/ExperienceModal.jsx
+++ src/components/ExperienceModal.jsx
@@ -193,7 +193,7 @@ export default function ExperienceModal({ open, onClose, experiences }) {
               <div>
                 <h3 className="text-lg font-semibold">Work experience roadmap</h3>
                 <p className="mt-1 text-sm text-zinc-200/80">
-                  Look at all my experiences, no matter what you throw at me; I will always get that done!
+                  These are all the organisations I have ever worked with. No matter what you throw at me; I will always come out on top!
                 </p>
               </div>
 
